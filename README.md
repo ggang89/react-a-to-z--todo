1714ce6ea6a8f343ef129529a3858c2d00b4b4af
ggang89 <ggang89@gmail.com>
2025-01-22 16:41:48 +0900

    할일 목록 UI만들기 => class component부터 연습

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 리액트 A to Z 들으면서 만드는 todoApp with typeScript
+
+1. class component로 먼저 만들어보기
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
+import React, { Component } from "react";
+import "./App.css";
 
-import './App.css'
+export default class App extends Component {
 
-function App() {
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float:"right",
+  }
  
-  return (
-    <>
-     
-    </>
-  )
-}
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration:"none"
+    }
+  }
 
-export default App
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button style ={this.btnStyle}>X</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
