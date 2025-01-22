c7c01f1e45188cdb847759df4f087293c04bcc21
ggang89 <ggang89@gmail.com>
2025-01-22 17:18:15 +0900

    map()으로 투두 그려주고, filter()로 투두 삭제하기(state 이용)

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,44 @@ import "./App.css";
 
 export default class App extends Component {
 
+  state = {
+     todoDatas : [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "운동하기",
+      completed: false,
+    },
+  ]
+  }
+
   btnStyle = {
     color: "#fff",
     border: "none",
     padding: "5px 9px",
     borderRadius: "50%",
     cursor: "pointer",
-    float:"right",
-  }
- 
+    float: "right",
+  };
+
   getStyle = () => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration:"none"
-    }
+      textDecoration: "none",
+    };
+  };
+
+  handleClick = (id:string) => {
+    const newTodoDatas = this.state.todoDatas.filter(todo => todo.id !== id);
+    this.setState({ todoDatas: newTodoDatas });
   }
+
+ 
 
   render() {
     return (
@@ -28,10 +50,15 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          <div style={this.getStyle()}>
-            <input type="checkbox" defaultChecked={false} />
-            공부하기
-            <button style ={this.btnStyle}>X</button>
+      
+          <div>
+            {this.state.todoDatas.map((todo) => (
+              <div key={todo.id} style={this.getStyle()}>
+                <input type="checkbox" defaultChecked={todo.completed} />
+               {" "} {todo.title}
+                <button style={this.btnStyle} onClick={()=>this.handleClick(todo.id)}>X</button>
+              </div>
+            ))}
           </div>
         </div>
       </div>
